741ebd0ba24f9ae64562f2ecbc936e89e76c7065
Marco Mamani <natmikhir@gmail.com>
2023-07-10 17:31:22 -0400

    closes #2;BaseButton and NavButton component creation

---
+++ src/components/BaseButton.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const BaseButton = ({ color, text }) => {
+   return (
+      <button
+         style={{
+         color: color,
+         backgroundColor: "white",
+         border: "1px solid #ccc",
+         padding: "10px 20px",
+         }}
+      >
+         {text}
+      </button>
+   );
+};
+
+export default BaseButton;
---
+++ src/components/nav/NavButton.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import BaseButton from '../BaseButton'
+
+const NavButton = ({ color, text, to }) => {
+   return (
+      <BaseButton
+         color={color}
+         text={text}
+         onClick={() => {
+            window.location.href = to;
+         }}
+      >
+         {text}
+      </BaseButton>
+      );
+   };
+
+export default NavButton;
